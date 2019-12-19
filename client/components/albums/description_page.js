218e7fb30c5a7cb76a3e3d5f062f78e35022aec5
Christian Gerace <cgerace8@gmail.com>
2019-12-18 22:11:10 -0500

    Worked on album detail page alittle

--- client/components/albums/description_page.js
+++ client/components/albums/description_page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {getAlbum} from '../../store'
+import {List, Dropdown, Button} from 'semantic-ui-react'
 
 const stateProps = state => {
   return {
@@ -20,9 +21,38 @@ class AlbumPage extends React.Component {
   }
 
   render() {
+    const options = [
+      {key: 1, text: '1', value: 1},
+      {key: 2, text: '2', value: 2},
+      {key: 3, text: '3', value: 3},
+      {key: 4, text: '4', value: 4}
+    ]
+
     return (
-      <div>
-        <h1>{this.props.album.title}</h1>
+      <div id="album-page">
+        <div id="album-display">
+          <img src={this.props.album.imageUrl} />
+        </div>
+        <div id="album-info">
+          <List>
+            <List.Item>
+              <strong>Title:</strong> {this.props.album.title}
+            </List.Item>
+            <List.Item>
+              <strong>Artist:</strong> {this.props.album.artist}
+            </List.Item>
+            <List.Item>
+              <strong>Year:</strong> {this.props.album.year}
+            </List.Item>
+            <List.Item>
+              <strong>Genre:</strong> {this.props.album.genre}
+            </List.Item>
+          </List>
+          <Dropdown placeholder="Quantity" fluid selection options={options} />
+          <Button id="add-to-cart" primary>
+            Add to Cart
+          </Button>
+        </div>
       </div>
     )
   }

e94cd1c9cb77fd9c08b24b74ddd3dd6999e52add
Christian Gerace <cgerace8@gmail.com>
2019-12-22 16:38:52 -0500

    Created a route for showing users orders

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {User, Order} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -10,5 +10,37 @@ router.get('/', async (req, res, next) => {
     res.json(users)
   } catch (err) {
     next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id)
+    if (user) {
+      res.json(user)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id/orders', async (req, res, next) => {
+  try {
+    const orders = await User.findByPk(req.params.id, {
+      include: [
+        {
+          model: Order,
+          where: {
+            status: 'Complete'
+          }
+        }
+      ]
+    })
+
+    if (orders) {
+      res.json(orders)
+    }
+  } catch (error) {
+    next(error)
   }
 })
